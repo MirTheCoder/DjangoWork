2ad635a8b8974660ae7b95417a59d8060ec48f1a
MirTheCoder <mokoli142@gmail.com>
2025-04-19 22:49:32 -0400

    Started setting up my django project

--- djangoProd/blog/views.py
+++ djangoProd/blog/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1> Blog home </h1>")
+    return HttpResponse("<doctype>...")
 
 def about(request):
     return HttpResponse('<h1> Blog About </h1>')
+
