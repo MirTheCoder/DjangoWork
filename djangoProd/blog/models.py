59bce4e9e7ff8e0c8ae5cdcdf74efc744f8227cb
MirTheCoder <mokoli142@gmail.com>
2025-04-23 14:05:39 -0400

    added a new table to our sql database

--- djangoProd/blog/models.py
+++ djangoProd/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+#We are going to inherit the author for our post model data table from the User data table made by django
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Side note: Remember to do python djangoProd/manage.py makemigrations to update any changes made to the database
+#Each Class is its own table in the database
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    #This will delete the post made for this model/table if the user is deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+#Don't forget to do djangoProd/manage.py migrate in order to execute those changes to the database
