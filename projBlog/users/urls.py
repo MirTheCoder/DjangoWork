7e447926dcf70ac3aa732ff051ec520ddcb589fa
Miracle Okoli <mokoli142@gmal.com>
2025-06-21 00:53:49 -0400

    So I have established the users app and a template where users can now register (however, I haven't yet created a system in which the users input data is saved to the database). I have made some edits to the url patterns to include the users app and making navigation a little smoother.

---
+++ projBlog/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+#These url patterns help decided which function in our views file handle the request
+urlpatterns = [
+    #We leave the end of the url blank to navigate to our home page
+    #We set the view to views.home which tells django to access the home function in our views pytho file if the home
+    #page is requested
+    #Because we can potentially have multiple home pages, we need to name the blog one "blog-home" to avoid confusion
+    path('register/', views.register, name='users-register'),
+    path('', views.home, name='users-home'),
+]
