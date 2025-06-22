2541a4dfb29f2e2da873c81ec80b64f78204b356
Miracle Okoli <mokoli142@gmal.com>
2025-06-22 16:42:46 -0400

    We have made some edits in the way we are displaying our users blogs. We have made a system for them to edit their accounts and are working on a feature that will let them also create enw blogs

--- projBlog/blog/urls.py
+++ projBlog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView
 
 #These url patterns help decided which function in our views file handle the request
 urlpatterns = [
@@ -7,6 +8,11 @@ urlpatterns = [
     #We set the view to views.home which tells django to access the home function in our views pytho file if the home
     #page is requested
     #Because we can potentially have multiple home pages, we need to name the blog one "blog-home" to avoid confusion
-    path('', views.home, name='blog-home'),
+    #Also, remember to do PostListView.as_view() to pass the class view PostListView as an actual view since it is not
+    #a view on its own
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    #We are going to use this url to go see the details of a selected Blog
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
--- projBlog/users/forms.py
+++ projBlog/users/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from.models import Profile
 
 #We are creating a form that inherits
 class UserRegisterForm(UserCreationForm):
+    #We are adding an email field for the user to input their email when creating their account
     email = forms.EmailField(required=False)
 
     #Specifies the model or database this form interacts with
@@ -11,3 +13,22 @@ class UserRegisterForm(UserCreationForm):
         model = User
         #This shows the fields the user needs to fill out in order to create their account
         fields = ["username", "email", "password1", "password2"]
+
+#This will be used to allow the user to update their username and password
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=False)
+
+    # Specifies the model or database this form interacts with
+    class Meta:
+        model = User
+        # These are the fields that we are allowing the user to update
+        fields = ["username", "email"]
+
+#We will use this model to update a users profile
+class ProfileUpdateForm(forms.ModelForm):
+
+    # Specifies the model or database this form interacts with
+    class Meta:
+        model = Profile
+        # These are the fields that we are allowing the
+        fields = ["image"]
